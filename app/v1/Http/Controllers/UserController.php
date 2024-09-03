b58f4ea99a3d78869bd8cd169ac66aacdc787f4b
Sergey <obrazcovsergey@gmail.com>
2024-09-03 15:05:16 +0300

    add users controllers and routes

---
+++ app/v1/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\v1\Http\Controllers;
+
+use App\v1\Http\Resources\UserResource;
+use App\v1\Models\User;
+use App\v1\Http\Requests\StoreUserRequest;
+use App\v1\Http\Requests\UpdateUserRequest;
+
+class UserController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return UserResource::collection(
+            User::query()->orderBy('id', 'desc')->get()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+        return response(new UserResource($user), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $data = $request->validated();
+        if(isset($data['password'])){
+            $data['password'] = bcrypt($data['password']);
+        }
+        $user->update($data);
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response('', 204);
+    }
+}
--- routes/v1/routes.php
+++ routes/v1/routes.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\v1\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('sanctum')->as('sanctum:')->group(
     base_path('routes/v1/auth.php'),
 );
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('/users',UserController::class);
+});
 
 Route::get('/unauthenticated', function () {
     return response()->json(
