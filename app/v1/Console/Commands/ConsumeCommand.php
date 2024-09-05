a7bd5ddb7d3484bbac7056849472bfc57cf9804b
Sergey <obrazcovsergey@gmail.com>
2024-09-05 15:43:31 +0300

    add command and work rabbit with docker

---
+++ app/v1/Console/Commands/ConsumeCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\v1\Console\Commands;
+
+use Illuminate\Console\Command;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+class ConsumeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rabbitmq:consume';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('hello', false, true, false, false);
+
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+        };
+
+        $channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+        try {
+            $channel->consume();
+        } catch (\Throwable $exception) {
+            echo $exception->getMessage();
+        }
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,4 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withCommands([
+        __DIR__.'/../app/v1/Console',
+    ])->create();
